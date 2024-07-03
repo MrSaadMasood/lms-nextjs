7fddbbe175064b4241e172cee414f4331d58762e
MrSaadMasood <mrsaadmasood1@gmail.com>
2024-07-03 17:36:53 +0500

    feat : added authjs env for deployment

--- src/lib/envValidator.ts
+++ src/lib/envValidator.ts
@@ -3,21 +3,23 @@ import { z } from "zod"
 const envSchema = z.object({
   AUTH_GOOGLE_SECRET: z.string(),
   AUTH_GOOGLE_ID: z.string(),
-  BASE_URL: z.string(),
+  AUTH_URL: z.string(),
   AUTH_SECRET: z.string(),
   EMAIL: z.string().email(),
   POSTGRES_URL: z.string().optional(),
+  AUTH_TRUST_HOST: z.coerce.boolean()
 })
 
 const env = envSchema.parse(process.env)
-const { AUTH_SECRET, BASE_URL, EMAIL, AUTH_GOOGLE_SECRET, AUTH_GOOGLE_ID,
-  POSTGRES_URL } = env
+const { AUTH_SECRET, AUTH_URL, EMAIL, AUTH_GOOGLE_SECRET, AUTH_GOOGLE_ID,
+  POSTGRES_URL, AUTH_TRUST_HOST } = env
 
 export {
   AUTH_GOOGLE_ID,
   AUTH_GOOGLE_SECRET,
   AUTH_SECRET,
   EMAIL,
-  BASE_URL,
-  POSTGRES_URL
+  AUTH_URL,
+  POSTGRES_URL,
+  AUTH_TRUST_HOST
 }
