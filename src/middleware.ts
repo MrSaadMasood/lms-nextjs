b555763659d82eaa6419e64cccc646c7aef1854f
MrSaadMasood <mrsaadmasood1@gmail.com>
2024-07-23 14:53:33 +0500

    fix: route access based on the user role

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,25 @@
-import { auth } from "@/lib/authJs/auth"
+import { auth } from "@/lib/authJs/auth";
 
 export default auth((req) => {
+  if (!req.auth && req.nextUrl.pathname.startsWith("/dashboard/user"))
+    return Response.redirect(new URL("/login", req.nextUrl.origin));
+
   if (
-    !req.auth && req.nextUrl.pathname.startsWith("/dashboard/user"))
-    return Response.redirect(
-      new URL("/login", req.nextUrl.origin)
-    )
+    req.auth &&
+    req.auth.user.role === "user" &&
+    req.nextUrl.pathname.startsWith("/dashboard/admin")
+  )
+    return Response.redirect(new URL("/dashboard/user", req.nextUrl.origin));
 
-  if (req.auth && req.auth.user.role === "user" && req.nextUrl.pathname.startsWith("/dashboard/admin"))
-    return Response.redirect(new URL("/dashboard/user", req.nextUrl.origin))
-
-  if (req.auth && req.auth.user.role === "admin" && req.nextUrl.pathname.startsWith("/dashboard/user"))
-    return Response.redirect(new URL("/dashboad/admin", req.nextUrl.origin))
-})
+  if (
+    req.auth &&
+    req.auth.user.role === "admin" &&
+    req.nextUrl.pathname.startsWith("/dashboard/user")
+  )
+    return Response.redirect(new URL("/dashboad/admin", req.nextUrl.origin));
+});
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-  unstable_allowDynamic: [
-    "**/node_modules/@react-email/**/*.mjs"
-  ]
-}
-
+  unstable_allowDynamic: ["**/node_modules/@react-email/**/*.mjs"],
+};
