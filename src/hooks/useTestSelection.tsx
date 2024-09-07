ba4abbc68a4e024650ad051560fa07dd2c4e9a41
MrSaadMasood <mrsaadmasood1@gmail.com>
2024-09-07 23:22:50 +0500

    feat: add test selection hook for filtering and searching

---
+++ src/hooks/useTestSelection.tsx
@@ -0,0 +1,162 @@
+import { fetchCategorySpecificDataForFiltering } from "@/fetchRequests/fetch";
+import { generateCategoryCorrespondingObject, resetSelectedFiltersOptions, testSearchRelatedBooleanValuesGenerator } from "@/lib/utils/clientHelpers";
+import { dataForTestFilteringSchema } from "@/lib/zodSchema";
+import { useSearchParams } from "next/navigation";
+import { useCallback, useEffect, useMemo, useState } from "react";
+
+function useTestSelection(
+  {
+    category,
+    academyId,
+    errorToast
+  }: {
+    category: TestSearchCategory,
+    academyId: string,
+    errorToast: (value: string) => void
+  }
+) {
+
+  const [isFilterChanged, setIsFilterChanged] = useState(false)
+  const [selectedOption, setSelectedOption] = useState<SelectedOptionsMap<string>>({
+    category,
+    filter: "" as TestFilterByOptions,
+    exam: "",
+    year: "",
+    subject: "",
+    academy: "",
+    academyId
+  });
+  const [fetchedDataForTestFiltering, setFetchedDataForTestFiltering] = useState<DataForTestFiltering>({
+    examList: [],
+    subjectList: [],
+    academyList: [],
+    yearList: [],
+  })
+
+  const memoSelectedOption = useMemo(() => selectedOption, [selectedOption])
+  const memoFetcheDataForTestFiltering = useMemo(() => fetchedDataForTestFiltering, [fetchedDataForTestFiltering])
+
+  const searchParams = useSearchParams()
+
+  const {
+    isAcademyCategorySelected,
+    isSubjectCategorySelected,
+    isExamCategorySelected,
+    subjectChoosenAfterFilterExist,
+    examChoosenAfterFilterExist,
+    academyChoosenAfterFilterExist,
+    yearChoosenAfterFilterExist
+  } = testSearchRelatedBooleanValuesGenerator(selectedOption)
+
+
+
+  const changeValue = useCallback((value: string, type: string,) => {
+    const isFilterBeingChanged = type === "filter"
+    setSelectedOption((prev) => {
+      if (isFilterBeingChanged) {
+        prev = resetSelectedFiltersOptions(prev)
+        setIsFilterChanged(true)
+      }
+      return {
+        ...prev,
+        [type]: value,
+      }
+    });
+  }, [])
+
+  const stopCategroySpecificDataFetcherFromRunning = useCallback(() => {
+    if ((isAcademyCategorySelected || isSubjectCategorySelected) && !isFilterChanged) return true
+    if (isExamCategorySelected && memoFetcheDataForTestFiltering.subjectList.length > 1) return true
+    return false
+
+  }, [
+    isFilterChanged,
+    memoFetcheDataForTestFiltering,
+    isAcademyCategorySelected,
+    isSubjectCategorySelected,
+    isExamCategorySelected,
+  ])
+
+  const getCategorySpecificDataForFurtherFiltering = useCallback(
+    async () => {
+      try {
+        const { filter, subject, exam, academyId, category } = memoSelectedOption
+        if (stopCategroySpecificDataFetcherFromRunning()) return
+        const queryParams = `category=${category}&exam=${exam}&filter=${filter}&subject=${subject}&academyId=${academyId}`
+        const response = await fetchCategorySpecificDataForFiltering(queryParams)
+        if (!response.ok) throw new Error(response.statusText);
+        const data = await response.json();
+        const dataForTestFiltering = dataForTestFilteringSchema.parse(data)
+        setFetchedDataForTestFiltering((pre) => ({
+          ...pre,
+          ...dataForTestFiltering
+        }))
+      } catch (error) {
+        console.log('the error occured while getting further data for test filteration', error)
+        errorToast("Failed To Get Further Filter Options")
+      } finally {
+        setIsFilterChanged(false)
+      }
+    }, [
+    stopCategroySpecificDataFetcherFromRunning,
+    errorToast,
+    memoSelectedOption,
+  ])
+
+  useEffect(() => {
+    getCategorySpecificDataForFurtherFiltering()
+
+  }, [getCategorySpecificDataForFurtherFiltering])
+
+
+
+  const categoryCorrespondingMappedObject: CategoryCorrespondingMappedObject = generateCategoryCorrespondingObject({
+    fetchedDataForTestFiltering,
+    selectedOption
+  })
+
+
+  function createParamsForTestPageNavigation() {
+    const params = new URLSearchParams(searchParams)
+    const { filter, academy, exam, subject, year } = selectedOption
+    params.set("filter", filter);
+    params.set("academy", academy);
+    params.set("subject", subject);
+    params.set("exam", exam);
+    params.set("year", year);
+    return params
+  }
+
+
+  function stopUserIfIncompleteOptionsSelected() {
+    const isGeneralFilterSelected = selectedOption.filter
+    switch (category) {
+      case "academy":
+        if (
+          !isGeneralFilterSelected ||
+          (!subjectChoosenAfterFilterExist && !examChoosenAfterFilterExist)
+        ) return true
+        else return false
+      case "exam":
+        if (!subjectChoosenAfterFilterExist && !yearChoosenAfterFilterExist) return true;
+        else return false
+      case "subject":
+        if (
+          !isGeneralFilterSelected || (
+            !academyChoosenAfterFilterExist && !yearChoosenAfterFilterExist
+          )
+        ) return true
+        else return false
+    }
+  }
+
+  return {
+    categoryCorrespondingMappedObject,
+    createParamsForTestPageNavigation,
+    stopUserIfIncompleteOptionsSelected,
+    changeValue,
+    selectedOption
+  }
+}
+
+export default useTestSelection
