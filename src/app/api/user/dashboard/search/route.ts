9ba92bedc4604a8d7f1da3dacf84ab68a7f89c82
MrSaadMasood <mrsaadmasood1@gmail.com>
2024-09-07 23:20:30 +0500

    fix: convert POST route to GET

--- src/app/api/user/dashboard/search/route.ts
+++ src/app/api/user/dashboard/search/route.ts
@@ -1,31 +1,27 @@
 import { auth } from "@/lib/authJs/auth";
 import { CustomNextRequest } from "@/lib/types/nextRequest";
-import { testSelectedOptionZodSchema, zodString } from "@/lib/zodSchema";
-import { examTestsOfferedByAcademy, subjectTestOfferedByAcademy } from "@/SQLqueries/userQueries";
+import { testSelectedOptionZodSchema } from "@/lib/zodSchema";
+import { academiesThatOfferSubject, examTestsOfferedByAcademy, paperYearListOfSubjectFromAllAcademies, subjectListPresentInExam, subjectTestOfferedByAcademy } from "@/SQLqueries/userQueries";
 import { NextResponse } from "next/server";
 
 export const GET = auth(async function GET(req: CustomNextRequest) {
   try {
-    const session = req.auth;
-    const body = await req.json();
-    console.log("the req body is", body);
-    const parseSelectedOptions = testSelectedOptionZodSchema.safeParse(body)
+    const searchParams = req.nextUrl.searchParams
+    const selectedOptions = Object.fromEntries(searchParams.entries())
+
+    const parseSelectedOptions = testSelectedOptionZodSchema.safeParse(selectedOptions)
     if (parseSelectedOptions.success) {
 
-      const dataForTestFiltering: {
-        examList: { paper_category: string, paper_year: number }[],
-        subjectList: { subject: string, paper_year: number }[],
-        academyList: { academy_name: string, paper_year: number }[]
-      } = {
+      const dataForTestFiltering: DataForTestFiltering = {
         examList: [],
         subjectList: [],
         academyList: [],
+        yearList: []
       }
       const selectedOptions = parseSelectedOptions.data
-      console.log("the selected Options is", selectedOptions)
       switch (selectedOptions.category) {
         case "academy":
-          if (selectedOptions.filter === "exam") {
+          if (selectedOptions.filter.toLowerCase() === "exam") {
             dataForTestFiltering.examList = await examTestsOfferedByAcademy(selectedOptions.academyId)
           }
           else {
@@ -33,10 +29,14 @@ export const GET = auth(async function GET(req: CustomNextRequest) {
           }
           break;
         case "exam":
-
-
-
-        default:
+          dataForTestFiltering.subjectList = await subjectListPresentInExam(selectedOptions.exam)
+          break;
+        case "subject":
+          if (selectedOptions.filter.toLowerCase() === "academy") {
+            dataForTestFiltering.academyList = await academiesThatOfferSubject(selectedOptions.subject)
+          } else {
+            dataForTestFiltering.yearList = await paperYearListOfSubjectFromAllAcademies(selectedOptions.subject)
+          }
           break;
       }
       return NextResponse.json(dataForTestFiltering)
