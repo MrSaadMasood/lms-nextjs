471cc61cdb651640f53d8f31eb2f10dffea8ad23
MrSaadMasood <mrsaadmasood1@gmail.com>
2024-10-09 01:58:19 +0500

    feat: add schemas for real time route

--- src/lib/clientEnvValidator.ts
+++ src/lib/clientEnvValidator.ts
@@ -1,16 +1,17 @@
-import "client-only"
 import { z } from "zod";
-
-
 const envSchema = z.object({
   NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: z.string().min(1),
-});
+  NEXT_PUBLIC_COIN_GECKO_API_KEY: z.string().min(1)
+})
 
 const env = envSchema.parse(process.env);
+
 const {
   NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
-} = env;
+  NEXT_PUBLIC_COIN_GECKO_API_KEY
+} = env
 
 export {
   NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
-};
+  NEXT_PUBLIC_COIN_GECKO_API_KEY
+}
